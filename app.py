b1fbd710f82b093250e771d555d63bd94bdda6b3
QGBSA <qgbsa555@gmail.com>
2024-10-17 19:39:12 +0800

    update app.py and index.html

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Get the CONTRACT_ADDRESS from the environment variable
-    contract_address = os.getenv("CONTRACT_ADDRESS")
-    return render_template("index.html", contract_address=contract_address)
+    return(render_template("index.html"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
