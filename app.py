f2b8e0322f7a03165f5a1bd396c7f1312307b917
RWU <45728791+QGBSA@users.noreply.github.com>
2024-10-17 19:43:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return(render_template("index.html"))
+    # Get the CONTRACT_ADDRESS from the environment variable
+    contract_address = os.getenv("CONTRACT_ADDRESS")
+    return render_template("index.html", contract_address=contract_address)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
